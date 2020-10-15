1ce94dad352fb35eb5b41a8458b003980d78233f
zhuoqian <yzhq97@gmail.com>
2020-10-15 11:53:37 -0400

    test

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,4 +1,4 @@
 python scripts/preprocess.py --data_dir data/mixamo/36_800_24/train -i 32 -s 64 -p 0
 python scripts/preprocess.py --data_dir data/mixamo/36_800_24/test -i 60 -s 120 -p 0
 python scripts/rotate_test_set.py --data_dir data/mixamo/36_800_24/test --out_dir data/mixamo/36_800_24/test_random_rotate
-python scripts/data/preprocess_solo_dance.py --data_dir data/solo_dance/train -i 32 -s 64 -p 0
+python scripts/preprocess_solo_dance.py --data_dir data/solo_dance/train -i 32 -s 64 -p 0
